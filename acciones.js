abc84f8b02b6af078afc8e9cf0e11257e519c364
ElkiMontes <elkinmonteskv1@gmail.com>
2026-09-24 14:14:46 -0600

    modify js

--- acciones.js
+++ acciones.js
@@ -71,7 +71,7 @@ btnEnviar.addEventListener("click", function () {
     };
 
     
-    fetch("https://motorep-api-hyazbuagbvg3cvbj.centralus-01.azurewebsites.net//api/Contactos", {
+    fetch("appservice-motorepapi-hibrid-hzfde3a9cnfmfaaq.mexicocentral-01.azurewebsites.net//api/Contactos", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data)
